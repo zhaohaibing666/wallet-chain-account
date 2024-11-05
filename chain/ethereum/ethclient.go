6d5fa1b7ed433e56a01d9fe667cbb9c8d9044b94
zhaohaibing <zhaohaibing@oneniceapp.com>
2024-11-05 18:33:38 +0800

    ethclient

--- chain/ethereum/ethclient.go
+++ chain/ethereum/ethclient.go
@@ -7,6 +7,7 @@ import (
 	"math/big"
 	"net"
 	"net/url"
+	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -14,6 +15,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/zhaohaibing666/wallet-chain-account/common/global_const"
 	"github.com/zhaohaibing666/wallet-chain-account/common/retry"
 )
 
@@ -69,28 +71,141 @@ type Clnt struct {
 }
 
 // BlockByHash implements EthClient.
-func (c *Clnt) BlockByHash(common.Hash) (*RpcBlock, error) {
-	panic("unimplemented")
+func (c *Clnt) BlockByHash(hash common.Hash) (*RpcBlock, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var block *RpcBlock
+	err := c.rpc.CallContext(ctxwt, &block, "eth_getBlockByHash", hash, true)
+	if err != nil {
+		return nil, err
+	}
+	if block == nil {
+		return nil, ethereum.NotFound
+	}
+
+	return block, nil
 }
 
 // BlockByNumber implements EthClient.
-func (c *Clnt) BlockByNumber(*big.Int) (*RpcBlock, error) {
-	panic("unimplemented")
+func (c *Clnt) BlockByNumber(number *big.Int) (*RpcBlock, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var block *RpcBlock
+	err := c.rpc.CallContext(ctxwt, &block, "eth_getBlockByNumber", toBlockNumArg(number), true)
+	if err != nil {
+		return nil, err
+	}
+	if block == nil {
+		return nil, ethereum.NotFound
+	}
+
+	return block, nil
 }
 
 // BlockHeaderByHash implements EthClient.
-func (c *Clnt) BlockHeaderByHash(common.Hash) (*types.Header, error) {
-	panic("unimplemented")
+func (c *Clnt) BlockHeaderByHash(hash common.Hash) (*types.Header, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+
+	var header *types.Header
+	err := c.rpc.CallContext(ctxwt, &header, "eth_getBlockByHash", hash, false)
+	if err != nil {
+		return nil, err
+	}
+	if header == nil {
+		return nil, ethereum.NotFound
+	}
+
+	if header.Hash() != hash {
+		return nil, errors.New("hash mismatch")
+	}
+	return header, nil
 }
 
 // BlockHeaderByNumber implements EthClient.
-func (c *Clnt) BlockHeaderByNumber(*big.Int) (*types.Header, error) {
-	panic("unimplemented")
+func (c *Clnt) BlockHeaderByNumber(number *big.Int) (*types.Header, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var head *types.Header
+	err := c.rpc.CallContext(ctxwt, &head, "eth_getBlockByNumber", toBlockNumArg(number), false)
+	if err != nil {
+		return nil, err
+	}
+	if head == nil {
+		return nil, ethereum.NotFound
+	}
+
+	return head, nil
 }
 
 // BlockHeadersByRange implements EthClient.
-func (c *Clnt) BlockHeadersByRange(*big.Int, *big.Int, uint) ([]types.Header, error) {
-	panic("unimplemented")
+func (c *Clnt) BlockHeadersByRange(startHeight *big.Int, endHeight *big.Int, chainId uint) ([]types.Header, error) {
+	if startHeight.Cmp(endHeight) == 0 {
+		header, err := c.BlockHeaderByNumber(startHeight)
+		if err != nil {
+			return nil, err
+		}
+		return []types.Header{*header}, nil
+	}
+	count := new(big.Int).Sub(endHeight, startHeight).Uint64() + 1
+	headers := make([]types.Header, count)
+	batchElems := make([]rpc.BatchElem, count)
+
+	cxtwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+
+	if chainId == uint(global_const.ZkFairSepoliaChainId) || chainId == uint(global_const.ZkFairChainId) {
+		groupSize := 100
+		var wg sync.WaitGroup
+		numGroup := int(count-1)/groupSize + 1
+		wg.Add(numGroup)
+
+		for i := 0; i < int(count); i += groupSize {
+			start := i
+			end := i + groupSize - 1
+			if end > int(count) {
+				end = int(count) - 1
+			}
+			go func(start, end int) {
+				defer wg.Done()
+				for j := 0; j <= end; j++ {
+					height := new(big.Int).Add(startHeight, new(big.Int).SetUint64(uint64(j)))
+					batchElems[j] = rpc.BatchElem{
+						Method: "eth_getBlockByNumber",
+						Result: new(types.Header),
+						Error:  nil,
+					}
+					header := new(types.Header)
+					batchElems[j].Error = c.rpc.CallContext(cxtwt, header, batchElems[j].Method, toBlockNumArg(height), false)
+					batchElems[j].Result = header
+				}
+			}(start, end)
+		}
+		wg.Wait()
+	} else {
+		for i := uint64(0); i < count; i++ {
+			height := new(big.Int).Add(startHeight, new(big.Int).SetUint64(i))
+			batchElems[i] = rpc.BatchElem{
+				Method: "eth_getBlockByNumber",
+				Args:   []interface{}{toBlockNumArg(height), false},
+				Result: &headers[i],
+			}
+			err := c.rpc.BatchCallContext(cxtwt, batchElems)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	for index, batchElem := range batchElems {
+		header, ok := batchElem.Result.(*types.Header)
+		if !ok {
+			continue
+		}
+		headers[index] = *header
+	}
+
+	return headers, nil
 }
 
 // Close implements EthClient.
@@ -99,53 +214,159 @@ func (c *Clnt) Close() {
 }
 
 // FilterLogs implements EthClient.
-func (c *Clnt) FilterLogs(filterQuery ethereum.FilterQuery, chainId uint) (Logs, error) {
-	panic("unimplemented")
+func (c *Clnt) FilterLogs(query ethereum.FilterQuery, chainId uint) (Logs, error) {
+	arg, err := toFilterArg(query)
+	if err != nil {
+		return Logs{}, err
+	}
+
+	var logs []types.Log
+	var header types.Header
+
+	batchElems := make([]rpc.BatchElem, 2)
+	batchElems[0] = rpc.BatchElem{Method: "eth_getBlockByNumber", Args: []interface{}{toBlockNumArg(query.ToBlock), false}, Result: &header}
+	batchElems[1] = rpc.BatchElem{Method: "eth_getLogs", Args: []interface{}{arg}, Result: &logs}
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout*10)
+	defer cancel()
+	if chainId == uint(global_const.ZkFairSepoliaChainId) ||
+		chainId == uint(global_const.ZkFairChainId) {
+
+		batchElems[0].Error = c.rpc.CallContext(ctxwt, &header, batchElems[0].Method, toBlockNumArg(query.ToBlock), false)
+		batchElems[1].Error = c.rpc.CallContext(ctxwt, &logs, batchElems[1].Method, arg)
+	} else {
+		err = c.rpc.BatchCallContext(ctxwt, batchElems)
+		if err != nil {
+			return Logs{}, err
+		}
+	}
+
+	if batchElems[0].Error != nil {
+		return Logs{}, fmt.Errorf("unable to query for the `FilterQuery#ToBlock` header: %w", batchElems[0].Error)
+	}
+	if batchElems[1].Error != nil {
+		return Logs{}, fmt.Errorf("unable to query logs: %w", batchElems[1].Error)
+	}
+	return Logs{Logs: logs, ToBlockHeader: &header}, nil
 }
 
 // LatestFinalizedBlockHeader implements EthClient.
 func (c *Clnt) LatestFinalizedBlockHeader() (*types.Header, error) {
-	panic("unimplemented")
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var header *types.Header
+	err := c.rpc.CallContext(ctxwt, &header, "eth_getBlockByNumber", "finalized", false)
+	if err != nil {
+		return nil, err
+	}
+	if header == nil {
+		return nil, ethereum.NotFound
+	}
+	return header, nil
 }
 
 // LatestSafeBlockHeader implements EthClient.
 func (c *Clnt) LatestSafeBlockHeader() (*types.Header, error) {
-	panic("unimplemented")
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var header *types.Header
+	err := c.rpc.CallContext(ctxwt, &header, "eth_getBlockByNumber", "safe", false)
+	if err != nil {
+		return nil, err
+	} else if header == nil {
+		return nil, ethereum.NotFound
+	}
+	return header, nil
 }
 
 // SendRawTransaction implements EthClient.
 func (c *Clnt) SendRawTransaction(rawTx string) error {
-	panic("unimplemented")
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	if err := c.rpc.CallContext(ctxwt, nil, "eth_sendRawTransaction", rawTx); err != nil {
+		return err
+	}
+	return nil
 }
 
 // StorageHash implements EthClient.
-func (c *Clnt) StorageHash(common.Address, *big.Int) (common.Hash, error) {
-	panic("unimplemented")
+func (c *Clnt) StorageHash(address common.Address, blockNumber *big.Int) (common.Hash, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+
+	proof := struct{ StorageHash common.Hash }{}
+	err := c.rpc.CallContext(ctxwt, &proof, "eth_getProof", address, nil, toBlockNumArg(blockNumber))
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	return proof.StorageHash, nil
 }
 
 // SuggestGasPrice implements EthClient.
 func (c *Clnt) SuggestGasPrice() (*big.Int, error) {
-	panic("unimplemented")
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var hex hexutil.Big
+	if err := c.rpc.CallContext(ctxwt, &hex, "eth_gasPrice"); err != nil {
+		return nil, err
+	}
+	return (*big.Int)(&hex), nil
 }
 
 // SuggestGasTipCap implements EthClient.
 func (c *Clnt) SuggestGasTipCap() (*big.Int, error) {
-	panic("unimplemented")
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var hex hexutil.Big
+	if err := c.rpc.CallContext(ctxwt, &hex, "eth_maxPriorityFeePerGas"); err != nil {
+		return nil, err
+	}
+	return (*big.Int)(&hex), nil
 }
 
 // TxByHash implements EthClient.
-func (c *Clnt) TxByHash(common.Hash) (*types.Transaction, error) {
-	panic("unimplemented")
+func (c *Clnt) TxByHash(hash common.Hash) (*types.Transaction, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+
+	var tx *types.Transaction
+	err := c.rpc.CallContext(ctxwt, &tx, "eth_getTransactionByHash", hash)
+
+	if err != nil {
+		return nil, err
+	} else if tx == nil {
+		return nil, ethereum.NotFound
+	}
+	return tx, nil
 }
 
 // TxCountByAddress implements EthClient.
-func (c *Clnt) TxCountByAddress(common.Address) (hexutil.Uint64, error) {
-	panic("unimplemented")
+func (c *Clnt) TxCountByAddress(address common.Address) (hexutil.Uint64, error) {
+	ctxwt, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout)
+	defer cancel()
+	var nonce hexutil.Uint64
+	err := c.rpc.CallContext(ctxwt, &nonce, "eth_getTransactionCount", address, "latest")
+	if err != nil {
+		return 0, err
+	}
+
+	return nonce, err
 }
 
 // TxReceiptByHash implements EthClient.
-func (c *Clnt) TxReceiptByHash(common.Hash) (*types.Receipt, error) {
-	panic("unimplemented")
+func (c *Clnt) TxReceiptByHash(hash common.Hash) (*types.Receipt, error) {
+	ctxwt, close := context.WithTimeout(context.Background(), defaultRequestTimeout.Truncate())
+	defer close()
+
+	var txReceipt *types.Receipt
+	err := c.rpc.CallContext(ctxwt, &txReceipt, "eth_getTransactionReceipt", hash)
+	if err != nil {
+		return nil, err
+	}
+	if txReceipt == nil {
+		return nil, ethereum.NotFound
+	}
+	return txReceipt, nil
 }
 
 func DiglEthClient(ctx context.Context, rpcUrl string) (EthClient, error) {
--- chain/ethereum/ethereum.go
+++ chain/ethereum/ethereum.go
@@ -2,11 +2,15 @@ package ethereum
 
 import (
 	"context"
+	"math/big"
 	"time"
+
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/zhaohaibing666/wallet-chain-account/chain"
 	"github.com/zhaohaibing666/wallet-chain-account/config"
-	"github.com/zhaohaibing666/wallet-chain-account/rpc/accout"
+	"github.com/zhaohaibing666/wallet-chain-account/rpc/account"
+	common2 "github.com/zhaohaibing666/wallet-chain-account/rpc/common"
 )
 
 const ChainName = "Ethereum"
@@ -17,92 +21,140 @@ type ChainAdaptor struct {
 }
 
 // BuildSignedTransaction implements chain.IChainAdaptor.
-func (c *ChainAdaptor) BuildSignedTransaction(*accout.SignedTransactionRequest) (*accout.SignedTransactionResponse, error) {
+func (c *ChainAdaptor) BuildSignedTransaction(*account.SignedTransactionRequest) (*account.SignedTransactionResponse, error) {
 	panic("unimplemented")
 }
 
 // ConvertAddress implements chain.IChainAdaptor.
-func (c *ChainAdaptor) ConvertAddress(*accout.ConvertAddressRequest) (*accout.ConvertAddressReponse, error) {
+func (c *ChainAdaptor) ConvertAddress(*account.ConvertAddressRequest) (*account.ConvertAddressResponse, error) {
 	panic("unimplemented")
 }
 
-// CreateUnsignTransaction implements chain.IChainAdaptor.
-func (c *ChainAdaptor) CreateUnsignTransaction(*accout.UnSignTransactionRequest) (*accout.UnSignTransactionResponse, error) {
+// CreateUnSignTransaction implements chain.IChainAdaptor.
+func (c *ChainAdaptor) CreateUnSignTransaction(*account.UnSignTransactionRequest) (*account.UnSignTransactionResponse, error) {
 	panic("unimplemented")
 }
 
 // DecodeTransaction implements chain.IChainAdaptor.
-func (c *ChainAdaptor) DecodeTransaction(*accout.DecodeTransactionRequest) (*accout.DecodeTransactionReponse, error) {
+func (c *ChainAdaptor) DecodeTransaction(*account.DecodeTransactionRequest) (*account.DecodeTransactionResponse, error) {
 	panic("unimplemented")
 }
 
 // GetAccount implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetAccount(*accout.AccountRequest) (*accout.AccountResponse, error) {
+func (c *ChainAdaptor) GetAccount(*account.AccountRequest) (*account.AccountResponse, error) {
 	panic("unimplemented")
 }
 
 // GetBlockByHash implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetBlockByHash(*accout.BlockHashRequest) (*accout.BlockReonse, error) {
-	panic("unimplemented")
+func (c *ChainAdaptor) GetBlockByHash(req *account.BlockHashRequest) (*account.BlockResponse, error) {
+	block, err := c.ethClient.BlockByHash(common.HexToHash(req.Hash))
+	if err != nil {
+		return &account.BlockResponse{
+			Code: common2.ReturnCode_ERROR,
+			Msg:  "blcok by hash error",
+		}, nil
+	}
+	var txListRet []*account.BlockInfoTransactionList
+	for _, v := range block.Transactions {
+		bitlItem := &account.BlockInfoTransactionList{
+			From:   v.From,
+			To:     v.To,
+			Amount: v.Value,
+			Hash:   v.Hash,
+		}
+		txListRet = append(txListRet, bitlItem)
+	}
+	return &account.BlockResponse{
+		Code:         common2.ReturnCode_SUCCESS,
+		Msg:          "block by hash success",
+		Height:       int64(block.Height),
+		Hash:         block.Hash.String(),
+		BaseFee:      block.BaseFee,
+		Transactions: txListRet,
+	}, nil
 }
 
 // GetBlockByNumber implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetBlockByNumber(*accout.BlockNumberRequest) (*accout.BlockReonse, error) {
-	panic("unimplemented")
+func (c *ChainAdaptor) GetBlockByNumber(req *account.BlockNumberRequest) (*account.BlockResponse, error) {
+	block, err := c.ethClient.BlockByNumber(big.NewInt(req.Height))
+	if err != nil {
+		return &account.BlockResponse{
+			Code: common2.ReturnCode_ERROR,
+			Msg:  "block by number error",
+		}, nil
+	}
+	var txListRet []*account.BlockInfoTransactionList
+	for _, v := range block.Transactions {
+		bitlItem := &account.BlockInfoTransactionList{
+			From:   v.From,
+			To:     v.To,
+			Hash:   v.Hash,
+			Amount: v.Value,
+		}
+		txListRet = append(txListRet, bitlItem)
+	}
+	return &account.BlockResponse{
+		Code:         common2.ReturnCode_SUCCESS,
+		Msg:          "block by number success",
+		Height:       int64(block.Height),
+		Hash:         block.Hash.String(),
+		BaseFee:      block.BaseFee,
+		Transactions: txListRet,
+	}, nil
 }
 
 // GetBlockByRange implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetBlockByRange(*accout.BlockByRangeRequest) (*accout.BlockByRangeReponse, error) {
+func (c *ChainAdaptor) GetBlockByRange(req *account.BlockByRangeRequest) (*account.BlockByRangeResponse, error) {
 	panic("unimplemented")
 }
 
 // GetBlockHeaderByHash implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetBlockHeaderByHash(*accout.BlockHeaderHashRequest) (*accout.BlockHeaderResponse, error) {
+func (c *ChainAdaptor) GetBlockHeaderByHash(req *account.BlockHeaderHashRequest) (*account.BlockHeaderResponse, error) {
 	panic("unimplemented")
 }
 
 // GetBlockHeaderByNumber implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetBlockHeaderByNumber(*accout.BlockHeaderNumberRequest) (*accout.BlockHeaderResponse, error) {
+func (c *ChainAdaptor) GetBlockHeaderByNumber(*account.BlockHeaderNumberRequest) (*account.BlockHeaderResponse, error) {
 	panic("unimplemented")
 }
 
 // GetExtraData implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetExtraData(*accout.ExtraDataRequest) (*accout.ExtraDataReponse, error) {
+func (c *ChainAdaptor) GetExtraData(*account.ExtraDataRequest) (*account.ExtraDataResponse, error) {
 	panic("unimplemented")
 }
 
 // GetFee implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetFee(*accout.FeeRequest) (*accout.FeeResponse, error) {
+func (c *ChainAdaptor) GetFee(*account.FeeRequest) (*account.FeeResponse, error) {
 	panic("unimplemented")
 }
 
 // GetSupportChains implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetSupportChains(*accout.SupportChainsRequest) (*accout.SupportChainsReponse, error) {
+func (c *ChainAdaptor) GetSupportChains(*account.SupportChainsRequest) (*account.SupportChainsResponse, error) {
 	panic("unimplemented")
 }
 
 // GetTxByAddress implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetTxByAddress(*accout.TxAddressRequest) (*accout.TxAddressResponse, error) {
+func (c *ChainAdaptor) GetTxByAddress(*account.TxAddressRequest) (*account.TxAddressResponse, error) {
 	panic("unimplemented")
 }
 
 // GetTxByHash implements chain.IChainAdaptor.
-func (c *ChainAdaptor) GetTxByHash(*accout.TxHashRequest) (*accout.TxHashResponse, error) {
+func (c *ChainAdaptor) GetTxByHash(*account.TxHashRequest) (*account.TxHashResponse, error) {
 	panic("unimplemented")
 }
 
 // SendTx implements chain.IChainAdaptor.
-func (c *ChainAdaptor) SendTx(*accout.SendTxRequest) (*accout.SendTxResponse, error) {
+func (c *ChainAdaptor) SendTx(*account.SendTxRequest) (*account.SendTxResponse, error) {
 	panic("unimplemented")
 }
 
-// ValidAdress implements chain.IChainAdaptor.
-func (c *ChainAdaptor) ValidAdress(*accout.ValidAddressRequest) (*accout.ValidAddressResponse, error) {
+// ValidAddress implements chain.IChainAdaptor.
+func (c *ChainAdaptor) ValidAddress(*account.ValidAddressRequest) (*account.ValidAddressResponse, error) {
 	panic("unimplemented")
 }
 
 // VerifySignedTransaction implements chain.IChainAdaptor.
-func (c *ChainAdaptor) VerifySignedTransaction(*accout.VerifySignedTxRequest) (*accout.VerifySignedTxResponse, error) {
+func (c *ChainAdaptor) VerifySignedTransaction(*account.VerifySignedTxRequest) (*account.VerifySignedTxResponse, error) {
 	panic("unimplemented")
 }
 
